214ce08e0e945557c0c315765e33e805fff893c1
Scar Dubu <scardubu@gmail.com>
2026-09-26 13:45:46 +0100

    fix(vercel): correct relative path to swarmx-api server in root serverless handler

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import server from "./apps/swarmx-api/src/server.js";
+import server from "../apps/swarmx-api/src/server.js";
 
 export default async function handler(req: IncomingMessage, res: ServerResponse): Promise<void> {
   await server.ready();
